0bb5b29c27a445e448a4707973f81c4ca0819f98
cehager <charlie80088@gmail.com>
2021-01-19 11:55:28 -0700

    end of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,5 @@
+using API.Helpers;
+using AutoMapper;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
@@ -28,8 +30,8 @@ namespace API
 
       services.AddControllers();
       services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-
-
+      services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+      services.AddAutoMapper(typeof(MappingProfiles));
 
       //   services.AddSwaggerGen(c =>
       //   {
@@ -50,6 +52,7 @@ namespace API
       app.UseHttpsRedirection();
 
       app.UseRouting();
+      app.UseStaticFiles();
 
       app.UseAuthorization();
 
