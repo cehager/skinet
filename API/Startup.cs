14136f03ebf7950e262394082f2c021c71432f0a
cehager <charlie80088@gmail.com>
2021-01-20 16:45:10 -0700

    end of section 5

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,13 +1,13 @@
+using API.Extensions;
 using API.Helpers;
+using API.Middleware;
 using AutoMapper;
-using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace API
 {
@@ -20,34 +20,22 @@ namespace API
 
     }
 
-
-
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-
-      services.AddScoped<IProductRepository, ProductRepository>();
-
       services.AddControllers();
       services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-      services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
       services.AddAutoMapper(typeof(MappingProfiles));
-
-      //   services.AddSwaggerGen(c =>
-      //   {
-      //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-      //   });
+      services.AddApplicationServices();
+      services.AddSwaggerDocumentation();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-        //     app.UseSwagger();
-        //     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-      }
+      app.UseMiddleware<ExceptionMiddleware>();
+
+      app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
       app.UseHttpsRedirection();
 
@@ -56,10 +44,12 @@ namespace API
 
       app.UseAuthorization();
 
+      app.UserSwaggerDocumention();
+
       app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers();
-      });
+     {
+       endpoints.MapControllers();
+     });
     }
   }
 }
